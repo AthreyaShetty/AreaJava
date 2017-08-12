0111a0dce59d197c3700337e99554899368d736e
Athreya <shettyathreya@gmail.com>
2017-08-12 18:00:30 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,12 @@
+//Compute the Area of the circle.
+class Area
+{
+	public static void main(String args[])
+	{
+		double pi,r,a;
+		r=10.8;		//Radius of a Circle.
+		pi=3.1416;	//pi, approximately.
+		a=pi*r*r;	//Compute Area.
+		System.out.println("Area of the Circle is "+a);
+	}
+}
